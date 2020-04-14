c3898a25c30d7f6b366456ff14385b8d88782fb4
Scott Ferguson <33704509+fergsc@users.noreply.github.com>
2020-04-14 10:43:06 +1000

    Update softMask.sh
    add a screening file

--- 8.Repeat_annotate/softMask.sh
+++ 8.Repeat_annotate/softMask.sh
@@ -1,12 +1,22 @@
+#!/bin/bash
+
+if [ -f softMask.lst ]; then
+    echo "softMask.lst exist - proceed"
+else
+    echo "softMask.lst does not exist and will be created. You should edit this, then rerun."
+    ls /g/data/xe2/scott/assembly/genomes/RaGOO/*.fasta | sort -h > softMask.lst
+    exit 0
+fi
+
 module load bedtools/2.28.0
 
-for fna in `ls /g/data/xe2/scott/assembly/genomes/RaGOO-masked/*.fasta`
+for fna in `cat softMask.lst`
 do
    fnaFile=$(basename $fna)
-   regions=`find /g/data/xe2/scott/assembly/genomes/RaGOO-masked -name "${fnaFile}.out" `
+   regions=`find /g/data/xe2/scott/assembly/genomes/RaGOO-hard -name "${fnaFile}.out" `
+   echo $fnaFile $regions
 
    sed -e '1,3d' $regions | awk -v OFS='\t' '{print $5, $6-1, $7}' > mask_regions.bed
-   echo $fnaFile $regions
    bedtools maskfasta -soft -fi $fna -bed mask_regions.bed -fo $fnaFile
 done
 
