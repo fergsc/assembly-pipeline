decca28b468bf1775f3c07d9a93903dbc6aef494
Scott Ferguson <33704509+fergsc@users.noreply.github.com>
2020-04-15 11:43:31 +1000

    Update 3.blob.sh

--- 3.contamination/3.blob.sh
+++ 3.contamination/3.blob.sh
@@ -7,16 +7,17 @@ if [ -f blobs.lst ]; then
 else 
     echo "blobs.lst does not exist and will be created. You should edit this, then rerun."
     find . -name 'blast.out' | grep '2.blast_contamination' > blobs.lst
-	exit 0
+        exit 0
 fi
 
 for blob in `cat blobs.lst`
 do
-	tmp=`echo $blob | awk '{split($0,a,"/"); print a[1] "/" a[2] "/" a[3]}'`
-	genome=`find $tmp -name '*.contigs.fasta'`
-	dir=`echo $blob | awk '{split($0,a,"/"); print "/g/data/xe2/scott/assembly/" a[1] "/" a[2] "/" a[3] "/" a[4]}'`
-	echo "cd $dir"
-	echo "/g/data/xe2/scott/gadi_modules/blobtools/blobtools create -i /g/data/xe2/scott/assembly/$genome -b sorted_mapped_reads.bam -t blast.out -o contamination"
-	echo "/g/data/xe2/scott/gadi_modules/blobtools/blobtools plot -i contamination.blobDB.json -o blobP"
-	echo "/g/data/xe2/scott/gadi_modules/blobtools/blobtools view -i contamination.blobDB.json -o blobV"
+    species=`echo $blob | cut -f2 -d'/'`
+    genome=`find $species -name '*.contigs.fasta'`
+    dir=`echo $blob | awk '{split($0,a,"/"); print "/g/data/xe2/scott/assembly/" a[1] "/" a[2] "/" a[3]}'`
+    echo "cd $dir"
+    echo "/g/data/xe2/scott/gadi_modules/blobtools/blobtools create -i /g/data/xe2/scott/assembly/${genome} -b sorted_mapped_reads.bam -t blast.out -o conta$"
+    echo "/g/data/xe2/scott/gadi_modules/blobtools/blobtools plot -i contamination.blobDB.json -o blobP"
+    echo "/g/data/xe2/scott/gadi_modules/blobtools/blobtools view -i contamination.blobDB.json -o blobV"
+    echo ""
 done
