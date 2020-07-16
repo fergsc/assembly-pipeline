3e0d073f784de8ca9e054d5670d721d3d88dfb24
Scott Ferguson <33704509+fergsc@users.noreply.github.com>
2020-07-16 17:04:56 +1000

    Create FindGenesInSyRI.py
    Takes a syri output file in the form species1~species2.out. 
    Loads in the associated and pre-processed species1.gtf and species2.gtf.
    Cycles through DUP, INV, INVDP, INVTR, SYN, TRANS syri events and files all genes that exist within the bounds of the syri event.
    Output =

---
+++ SyRI-Genes/FindGenesInSyRI.py
@@ -0,0 +1,172 @@
+import ntpath
+import os
+import sys
+import csv
+
+def loadGenes(filename):
+    tsv_file = open(filename)
+    geneFile = csv.reader(tsv_file, delimiter="\t")
+    chr01=[]
+    chr02=[]
+    chr03=[]
+    chr04=[]
+    chr05=[]
+    chr06=[]
+    chr07=[]
+    chr08=[]
+    chr09=[]
+    chr10=[]
+    chr11=[]
+    for gene in geneFile:
+        chrom = gene[0]
+        start = gene[1]
+        end = gene[2]
+        geneId = gene[3]
+        if chrom == "Chr01":
+            chr01.append([geneId, int(start), int(end)])
+        if chrom == "Chr02":
+            chr02.append([geneId, int(start), int(end)])
+        if chrom == "Chr03":
+            chr03.append([geneId, int(start), int(end)])
+        if chrom == "Chr04":
+            chr04.append([geneId, int(start), int(end)])
+        if chrom == "Chr05":
+            chr05.append([geneId, int(start), int(end)])
+        if chrom == "Chr06":
+            chr06.append([geneId, int(start), int(end)])
+        if chrom == "Chr07":
+            chr07.append([geneId, int(start), int(end)])
+        if chrom == "Chr08":
+            chr08.append([geneId, int(start), int(end)])
+        if chrom == "Chr09":
+            chr09.append([geneId, int(start), int(end)])
+        if chrom == "Chr10":
+            chr10.append([geneId, int(start), int(end)])
+        if chrom == "Chr11":
+            chr11.append([geneId, int(start), int(end)])
+    return [chr01, chr02, chr03, chr04, chr05, chr06, chr07, chr08, chr09, chr10, chr11]
+
+def inRegion(x, region):
+    if region[0] < region[1]:
+        return x > region[0] and x < region[1]
+    if region[0] > region[1]:
+        return x > region[1] and x < region[0]
+    return False
+
+syriFile = sys.argv[1] 
+refSpecies = ntpath.basename(syriFile).split("~")[0]
+qrySpecies = ntpath.basename(syriFile).split("~")[1]
+qrySpecies = qrySpecies.split(".")[0]
+
+if os.path.isfile(syriFile) == False:
+    print ("{} doesn't not exist".format(syriFile))
+    exit()
+
+if os.path.isfile("{}.genes".format(refSpecies)) == False:
+    print ("{}.genes doesn't not exist".format(refSpecies))
+    print("awk -v OFS='\\t' '{if($3 == \"gene\"){split($9,a,\";\"); if($4<$5){print $1,$4,$5,a[1]} else {print $1,$5,$4,a[1]}}}' XXX.gtf > XXX.genes")
+    exit()
+
+if os.path.isfile("{}.genes".format(qrySpecies)) == False:
+    print ("{}.genes doesn't not exist".format(qrySpecies))
+    print("awk -v OFS='\\t' '{if($3 == \"gene\"){split($9,a,\";\"); if($4<$5){print $1,$4,$5,a[1]} else {print $1,$5,$4,a[1]}}}' XXX.gtf > XXX.genes")
+    exit()
+
+print("ref:{}\nqry:{}".format(refSpecies, qrySpecies))
+
+#
+#open files
+tsv_file = open(syriFile)
+syriFile = csv.reader(tsv_file, delimiter="\t")
+
+refGenes = loadGenes("{}.genes".format(refSpecies))
+qryGenes = loadGenes("{}.genes".format(qrySpecies))
+
+#syriGenes = []
+refOutput = open("{}~{}~{}.syrigenes".format(refSpecies, qrySpecies, refSpecies),"w+")
+qryOutput = open("{}~{}~{}.syrigenes".format(refSpecies, qrySpecies, qrySpecies),"w+")
+
+refOutput.write("SyRIID,type,GeneId")
+qryOutput.write("SyRIID,type,GeneId")
+
+for syriEvent in syriFile:
+    # refChr = syriEvent[0]
+    # refStart = int(syriEvent[1])
+    # refEnd = int(syriEvent[2])
+    # qryChr = syriEvent[5]
+    # qryStart = int(syriEvent[6])
+    # qryEnd = int(syriEvent[7])
+    # uID = syriEvent[8]
+    # directionB = syriEvent[9]
+    # type = syriEvent[10]
+    if syriEvent[10] in ["DUP", "INV", "INVDP", "INVTR", "SYN", "TRANS"]:
+        if syriEvent[0] == "Chr01":
+            useChrA = 0
+        elif syriEvent[0] == "Chr02":
+            useChrA = 1
+        elif syriEvent[0] == "Chr03":
+            useChrA = 2
+        elif syriEvent[0] == "Chr04":
+            useChrA = 3
+        elif syriEvent[0] == "Chr05":
+            useChrA = 4
+        elif syriEvent[0] == "Chr06":
+            useChrA = 5
+        elif syriEvent[0] == "Chr07":
+            useChrA = 6
+        elif syriEvent[0] == "Chr08":
+            useChrA = 7
+        elif syriEvent[0] == "Chr09":
+            useChrA = 8
+        elif syriEvent[0] == "Chr10":
+            useChrA = 9
+        elif syriEvent[0] == "Chr11":
+            useChrA = 10
+        for gene in refGenes[useChrA]:
+            startIn =  inRegion(gene[1], [int(syriEvent[1]), int(syriEvent[2])])
+            endIn = inRegion(gene[2], [int(syriEvent[1]), int(syriEvent[2])])
+
+            if(startIn and endIn):
+                refOutput.write("{},{},{},inside\n".format(syriEvent[8], syriEvent[10], gene[0]))
+                
+            if(startIn and not endIn):
+                refOutput.write("{},{},{},start\n".format(syriEvent[8], syriEvent[10], gene[0]))
+
+            if(endIn and not startIn):
+                refOutput.write("{},{},{},end\n".format(syriEvent[8], syriEvent[10], gene[0]))
+
+        if syriEvent[5] == "Chr01":
+            useChrB = 0
+        elif syriEvent[5] == "Chr02":
+            useChrB = 1
+        elif syriEvent[5] == "Chr03":
+            useChrB = 2
+        elif syriEvent[5] == "Chr04":
+            useChrB = 3
+        elif syriEvent[5] == "Chr05":
+            useChrB = 4
+        elif syriEvent[5] == "Chr06":
+            useChrB = 5
+        elif syriEvent[5] == "Chr07":
+            useChrB = 6
+        elif syriEvent[5] == "Chr08":
+            useChrB = 7
+        elif syriEvent[5] == "Chr09":
+            useChrB = 8
+        elif syriEvent[5] == "Chr10":
+            useChrB = 9
+        elif syriEvent[5] == "Chr11":
+            useChrAB= 10
+
+        for gene in qryGenes[useChrB]:
+            startIn =  inRegion(gene[1], [int(syriEvent[6]), int(syriEvent[7])])
+            endIn = inRegion(gene[2], [int(syriEvent[6]), int(syriEvent[7])])
+
+            if(startIn and endIn):
+                qryOutput.write("{},{},{},inside\n".format(syriEvent[8], syriEvent[10], gene[0]))
+
+            if(startIn and not endIn):
+                qryOutput.write("{},{},{},start\n".format(syriEvent[8], syriEvent[10], gene[0]))
+
+            if(endIn and not startIn):
+                qryOutput.write("{},{},{},end\n".format(syriEvent[8], syriEvent[10], gene[0]))
