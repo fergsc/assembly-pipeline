4590bf21525aeab703f19b98f4812ed97a1063ad
Scott Ferguson <33704509+fergsc@users.noreply.github.com>
2020-07-27 12:47:32 +1000

    remove tests - commented out lines

--- Karyotype-plots/karyo-plot.R
+++ Karyotype-plots/karyo-plot.R
@@ -16,15 +16,12 @@ svg(sprintf("%s.svg", species), width = 20, height = 10)
 kp = plotKaryotype(genome = currGenome, main = species, plot.type=1)
 kpDataBackground(kp, data.panel = 1, r0 = 0, r1 = 0.48)
 kpDataBackground(kp, data.panel = 2, r0 = 0.52, r1 = 1)
-#kpAddBaseNumbers(kp, tick.dist = 10000000, tick.len = 10, tick.col="red", cex=1,
-#                      minor.tick.dist = 1000000, minor.tick.len = 5, minor.tick.col = "gray", add.units=TRUE)
+
 kpAxis(kp, ymin=0, ymax=22, cex=0.5, data.panel = 1, r0 = 0, r1 = 0.45) #, col="gray50", cex=0.5)
 kpAxis(kp, ymin=0, ymax=22, cex=0.5, data.panel = 1, r0 = 0.55, r1 = 1)
 
-#test = toGRanges(data.frame(chr = syriScores$chromosome, start = syriScores$chromosome))
 kpLines(kp, chr = syriScores$chromosome, col="red", x = syriScores$genomeIndex, y = syriScores$SYN/22,
         data.panel = 1, r0 = 0, r1 = 0.48)
-#kpLines(kp, chr = syriScores$chromosome, col="blue", x = syriScores$genomeIndex, y = syriScores$NOTAL/22)
 kpLines(kp, chr = syriScores$chromosome, col="blue", x = syriScores$genomeIndex, y = syriScores$REARRANGED/22,
         data.panel = 2, r0 = 0.52, r1 = 1)
 
